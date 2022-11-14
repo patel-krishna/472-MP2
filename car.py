3647adb6902408d1a918acf43b033a521bee6298
patel-krishna <krishna.patel0621@gmail.com>
2022-11-13 22:55:10 -0500

    setting up cars and getting their coordinates

--- car.py
+++ car.py
@@ -1,10 +1,15 @@
-class Car: 
+class Car(object): 
 
+    carsList = []
     def __init__(self, name, coordinates, position, fuel):
         self.name = name
-        self.coordinates = coordinates
+        self.coordinates = list(coordinates)
         self.position = position
         self.fuel = fuel
+        Car.carsList.append(self)
 
     def __str__(self): 
-        return f"{self.name}"
+        return f"{self.name}{self.coordinates}"
+
+    def set_coord(self,x, y):
+        self.coordinates.append((x,y))
--- driver.py
+++ driver.py
@@ -1,12 +1,50 @@
-import car
+from car import Car
 
 
-# Read input string 
+# Read input string, for now, 
+# lets work with a string, we can establish io later
+input = "..I...BBI.K.GHAAKLGHDDKLG..JEEFF.J.."
+
+if len(input)>36 or len(input)<36:
+    print('Input is wrong')
+else:
+    print("Input is valid")
 
 # place input string in multidim array (6x6)
+input_array = []
+
+counter =0
+while counter <36:
+    temp_columns =[]
+    for i in range(6): 
+        temp_columns.append(input[counter])
+        counter=counter+1
+    input_array.append(temp_columns)
+
+for i in range(6):
+    for j in range(6):
+        print(input_array[i][j], end=" ")
+    print("")
+
+
 
 # read multidimensional array by row and create horizontal cars
-
 # read md array by column and create vertical cars 
 
-# place car on board 
+
+
+cars_list = []
+
+for i in range(6):
+    for j in range(6):
+        if input_array[i][j] != ".":
+            for c in Car.carsList:
+                if input_array[i][j] == c.name:
+                    c.set_coord(i,j)
+                else:
+                    Car(input_array[i][j],(i,j),0,100)
+
+print(Car.carsList)
+
+
+# place car on board
