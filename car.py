b9aa58bdd7f3f86eee35629373300b41704f0e2b
rosiestr <75342579+rosiestr@users.noreply.github.com>
2022-11-15 22:16:15 -0500

    started move operation

--- car.py
+++ car.py
@@ -1,11 +1,12 @@
 class Car(object): 
 
     # carsList = []
-    def __init__(self, name, coordinates, position, fuel):
+    def __init__(self, name, coordinates, position, fuel,orientation):
         self.name = name
         self.coordinates = [coordinates]
         self.position = position
         self.fuel = fuel
+        self.orientation = orientation
         # Car.carsList.append(self)
 
     def __str__(self): 
@@ -13,3 +14,15 @@ class Car(object):
 
     def set_coord(self,x, y):
         self.coordinates.append((x,y))
+
+    def move(self, direction, spaces):
+        if self.fuel < spaces:
+            print("not enough fuel to move")
+        else:
+            #check if direction is valid
+            if direction == "right" or direction == "left" and self.orientation == "h":
+                print()
+            elif direction == "down" or direction == "up" and self.orientation == "v":
+                print()
+            else:
+                print("invalid move")
