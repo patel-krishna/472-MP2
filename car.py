0a44c9a45cdfb1b7a08703466d5806f062424840
rosiestr <75342579+rosiestr@users.noreply.github.com>
2022-11-24 11:36:28 -0500

    added adjust coord method

--- car.py
+++ car.py
@@ -1,11 +1,7 @@
 class Car(object): 
 
     # carsList = []
-<<<<<<< Updated upstream
-    def __init__(self, name, coordinates, position, fuel,orientation):
-=======
     def __init__(self, name, coordinates, fuel,orientation):
->>>>>>> Stashed changes
         self.name = name
         self.coordinates = [coordinates]
         self.fuel = fuel
@@ -15,22 +11,6 @@ class Car(object):
     def __str__(self): 
         return f"{self.name}{self.coordinates}"
 
-<<<<<<< Updated upstream
-    def set_coord(self,x, y):
-        self.coordinates.append((x,y))
-
-    def move(self, direction, spaces):
-        if self.fuel < spaces:
-            print("not enough fuel to move")
-        else:
-            #check if direction is valid
-            if direction == "right" or direction == "left" and self.orientation == "h":
-                print()
-            elif direction == "down" or direction == "up" and self.orientation == "v":
-                print()
-            else:
-                print("invalid move")
-=======
     def update_coord(self,x, y):
         self.coordinates.append((x,y))
 
@@ -48,4 +28,3 @@ class Car(object):
             for coord in self.coordinates:
                 coord[0] = coord[0]+1
 
->>>>>>> Stashed changes
