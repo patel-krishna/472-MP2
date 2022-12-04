e809e3f61225c933b97d80a8f0be0bae3a148d0a
patel-krishna <krishna.patel0621@gmail.com>
2022-12-04 11:16:00 -0500

    heuristics for astar

--- search.py
+++ search.py
@@ -6,25 +6,45 @@ import re
 import heapq
 from heuristics import Heuristics
 
+#============UCS
+
 class UCS: 
+
+# The shortestPath function is a dikjstra model search algorithm which will 
+# try to find the shortest path possible given a root node to a goal node 
+# The function returns the path solution to the path as well as the heursitics of each node
+    
     def shortestPath(root):
         h = []
 
         heapq.heappush(h, (0, root))
-        path = []
-        path.append(root)
+        ucs_path = []
+        ucs_values = []
+
+        ucs_path.append(root)
         while len(h) !=0:
             current_cost, current_node = heapq.heappop(h)
-            path.append(current_node)
+            ucs_path.append(current_node)
+            ucs_values.append((current_cost, current_cost, 0))
             if current_node.state[2][5] == "A" and current_node.state[2][4] == "A":
-                return path
+                return ucs_path, ucs_values 
             else:
                     for children in current_node.children:
                         heapq.heappush(h, (current_cost+children.cost, children.board))
-        return path
+        
+        return ValueError("There is no solution")
+
+# ============GREEDY
 
 class Greedy: 
-    # algorithm for greedy search, takes in be default heuristics 1
+
+    # The greedy algorithm is an algorithm that will find a search path
+    # depending on the heuristic of each node in the following layer
+    # it chooses the lowest value heuristic through a priority queue
+    # f(n) = h(n)
+    # the algorithm takes in the root node and a heuristic string input
+    # if no heurisitic choice is given, it will by default run h1
+
     def greedy(root, heur="h1"):
         # track of visited nodes
         visited = set()  
@@ -32,6 +52,8 @@ class Greedy:
         greedy_traversal = list()
         # queue
         queue = PriorityQueue()
+        # heuristics for traversal path 
+        greedy_values = list()
 
         # push the root node to the queue and mark it as visited 
         queue.put((0,root))
@@ -43,13 +65,14 @@ class Greedy:
                 # take the first board from the queue and add it to the traversal list
                 current_cost, current_node = queue.get()
                 greedy_traversal.append(current_node)
+                greedy_values.append((current_cost, 0, current_cost))
 
                 # update cost with heuristics 
-                current_cost = Heuristics.h1(current_node.state)
+                # current_cost = Heuristics.h1(current_node.state)
 
                 # if the current board is in a winning state, then return the bfs traversale
                 if current_node.state[2][5] == "A" and current_node.state[2][4] == "A" :
-                    return greedy_traversal
+                    return greedy_traversal, greedy_values
                 # else, check the children of that node 
                 else: 
                     for children in current_node.children:
@@ -65,13 +88,14 @@ class Greedy:
                 # take the first board from the queue and add it to the traversal list
                 current_cost, current_node = queue.get()
                 greedy_traversal.append(current_node)
+                greedy_values.append((current_cost, 0, current_cost))
 
                 # update cost with heuristics 
-                current_cost = Heuristics.h2(current_node.state)
+                # current_cost = Heuristics.h2(current_node.state)
 
                 # if the current board is in a winning state, then return the bfs traversale
                 if current_node.state[2][5] == "A" and current_node.state[2][4] == "A" :
-                    return greedy_traversal
+                    return greedy_traversal, greedy_values
                 # else, check the children of that node 
                 else: 
                     for children in current_node.children:
@@ -87,13 +111,15 @@ class Greedy:
                 # take the first board from the queue and add it to the traversal list
                 current_cost, current_node = queue.get()
                 greedy_traversal.append(current_node)
+                greedy_values.append((current_cost, 0, current_cost))
+
 
                 # update cost with heuristics 
-                current_cost = Heuristics.h3(current_node.state)
+                # current_cost = Heuristics.h3(current_node.state)
 
                 # if the current board is in a winning state, then return the bfs traversale
                 if current_node.state[2][5] == "A" and current_node.state[2][4] == "A" :
-                    return greedy_traversal
+                    greedy_traversal, greedy_values
                 # else, check the children of that node 
                 else: 
                     for children in current_node.children:
@@ -109,13 +135,14 @@ class Greedy:
                 # take the first board from the queue and add it to the traversal list
                 current_cost, current_node = queue.get()
                 greedy_traversal.append(current_node)
+                greedy_values.append((current_cost, 0, current_cost))
 
                 # update cost with heuristics 
-                current_cost = Heuristics.h4(current_node.state)
+                # current_cost = Heuristics.h4(current_node.state)
 
                 # if the current board is in a winning state, then return the bfs traversale
                 if current_node.state[2][5] == "A" and current_node.state[2][4] == "A" :
-                    return greedy_traversal
+                    return greedy_traversal, greedy_values
                 # else, check the children of that node 
                 else: 
                     for children in current_node.children:
@@ -129,12 +156,20 @@ class Greedy:
         return ValueError("There is no solution")
 
 
+# =============A/A*
+
 class ASTAR:
-    # algorithm for astar, takes h1 as default heuristics 
+
+    # astar algorithm takes in the root node of a graph and a heuristic input value 
+    # if no heuristic string is given, it will by default run on heuristic of h1
+    # the algorithm uses a priosity queue to choose the best node to traverse depending on the fn cost 
+    # f(n) = g(n) + h(n)
+    
     def astar(root, heur="h1"):
         open = PriorityQueue()
         closedset = set()
-        path = []
+        astar_path = list()
+        astar_values = list()
         current = root
 
         open.put((current, 0))
@@ -143,10 +178,14 @@ class ASTAR:
             while not open.empty(): 
                 # current_node, current_cost = min(openset, key=lambda o:o[0])
                 current_node, current_cost = open.get()
-                path.append(current_node)
+                astar_path.append(current_node)
+                fn = current_cost
+                hn = Heuristics.h1(current_node.state)
+                gn = fn-hn 
+                astar_values.append((fn, hn, gn))
                 
                 if current_node.state[2][5] == 'A' and current_node.state[2][4] == 'A':
-                    return path 
+                    return astar_path, astar_values
                 
                 # openset.remove((current_node, current_cost))
                 closedset.add(current_node)
@@ -161,11 +200,15 @@ class ASTAR:
             while not open.empty(): 
                 # current_node, current_cost = min(openset, key=lambda o:o[0])
                 current_node, current_cost = open.get()
-                path.append(current_node)
+                astar_path.append(current_node)
+                fn = current_cost
+                hn = Heuristics.h2(current_node.state)
+                gn = fn-hn 
+                astar_values.append((fn, hn, gn))
                 
                 if current_node.state[2][5] == 'A' and current_node.state[2][4] == 'A':
-                    path.append(current_node)
-                    return path 
+                    # astar_path.append(current_node)
+                    return astar_path, astar_values 
                 
                 # openset.remove((current_node, current_cost))
                 closedset.add(current_node)
@@ -181,11 +224,15 @@ class ASTAR:
             while not open.empty(): 
                 # current_node, current_cost = min(openset, key=lambda o:o[0])
                 current_node, current_cost = open.get()
-                path.append(current_node)
+                astar_path.append(current_node)
+                fn = current_cost
+                hn = Heuristics.h3(current_node.state)
+                gn = fn-hn 
+                astar_values.append((fn, hn, gn))
                 
                 if current_node.state[2][5] == 'A' and current_node.state[2][4] == 'A':
-                    path.append(current_node)
-                    return path 
+                    # astar_path.append(current_node)
+                    return astar_path, astar_values
                 
                 # openset.remove((current_node, current_cost))
                 closedset.add(current_node)
@@ -200,11 +247,15 @@ class ASTAR:
             while not open.empty(): 
                 # current_node, current_cost = min(openset, key=lambda o:o[0])
                 current_node, current_cost = open.get()
-                path.append(current_node)
+                astar_path.append(current_node)
+                fn = current_cost
+                hn = Heuristics.h4(current_node.state)
+                gn = fn-hn 
+                astar_values.append((fn, hn, gn))
                 
                 if current_node.state[2][5] == 'A' and current_node.state[2][4] == 'A':
-                    path.append(current_node)
-                    return path 
+                    # astar_path.append(current_node)
+                    return astar_path, astar_values
                 
                 # openset.remove((current_node, current_cost))
                 closedset.add(current_node)
